57e2b4f2037f6ff9bca6f9c4900a124f3e8025df
m67pg <m_owaki@nifty.com>
2025-06-21 01:08:58 +0900

    【改善】開発環境でも本番環境でも、なるべく変更なく動くように修正

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://backend.line-chat.fun',
+  baseURL: process.env.REACT_APP_BASE_URL,
   withCredentials: true,
 });
 
